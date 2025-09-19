22b40d91720a99d2621c020b83e498a0b3d758d8
Bruno Dutra Veleis <brunodutra2@hotmail.com>
2025-09-19 23:20:41 +0000

    Alterações nas mensagens iniciais e adicionado dois navios nas diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,10 +7,17 @@ int main() {
     //Variáveis do código
     int tabuleiro [10][10];
     char coluna[10] = {'A','B','C','D','E','F','G','H','I','J'};
-    int navio1 = 3, navio2 = 3;
+    int navio1 = 3, navio2 = 3, navio3 = 3, navio4 = 3;
 
     //Mensagem informativa sobre o jogo
-    printf("    ** TABULEIRO BATALHA NAVAL **\n");
+    printf(" =============================================\n");
+    printf("|        BEM-VINDO AO JOGO BATALHA NAVAL!     |\n");
+    printf(" =============================================\n");
+    printf("Seu tabuleiro está sendo montado com 4 navios.\n");
+    printf("Os navios estão representados pelo numeral '3' e a água pelo numeral '0'.\n");
+    printf("Os navios estão ocupando 3 posições: horizontal, vertical e diagonal.\n");
+    printf("\n");
+    printf("\n    ** TABULEIRO BATALHA NAVAL **\n");
     printf("\n");
     printf("    ");
 
@@ -22,24 +29,38 @@ int main() {
     }
 
     //Posicionamento do navio 1 na horizontal
-    if (tabuleiro[2][3]==0 && tabuleiro[2][4]==0 && tabuleiro[2][5]==0){
-        tabuleiro[2][3] = navio1;
-        tabuleiro[2][4] = navio1;
-        tabuleiro[2][5] = navio1;
+    for (int i=0; i<3; i++) {
+            tabuleiro[1][2+i] = navio1;
+
     }
 
     //Posicionamento do navio 2 na vertical
-    if (tabuleiro[6][7]==0 && tabuleiro[7][7]==0 && tabuleiro[8][7]==0){
-        tabuleiro[6][7] = navio2;
-        tabuleiro[7][7] = navio2;
-        tabuleiro[8][7] = navio2;
+    for (int j=0; j<3; j++) {
+        tabuleiro[6+j][8] = navio2;
+    }
+
+    //Posicionamento do navio 3 na diagonal
+    for (int y=4; y<7; y++) {
+        for (int j=1; j<4; j++) {
+            if (y+j==7) {
+                tabuleiro[y][j] = navio3;
+            }
+        }
+    }
+
+    //Posicionamento do navio 4 na diagonal
+    for (int y=0; y<8; y++) {
+        for (int j=6; j<9; j++) {
+            if (y-j==-5) {
+                tabuleiro[y][j] = navio4;
+            }
+        }
     }
 
     //Impressão do tabuleiro com os navios posicionados
     for (int x = 0; x < 10; x++) {
         printf(" %c ", coluna[x]);      //Impressão das colunas de A-J
-        }
-
+    }
     for (int y=0; y<10; y++) {
         printf("\n%2d  ", y+1);         //Impressão do número das linhas de 1-10
         for (int j=0; j<10; j++) {
@@ -48,6 +69,6 @@ int main() {
     }
 
     printf("\n");
-    
+
     return 0;
 }
